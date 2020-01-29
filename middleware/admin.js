1b3479c59aa5650c61507b1c1aeb20f9c6881cd2
molk95 <molk.saouabi@gmail.com>
2020-01-29 19:10:23 +0100

    ok

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,4 +1,4 @@
 module.exports = function(req, res, next) {
-    if (req.user.role !== 'Admin') return res.status(403).send('Access Denied');
+    if (req.user.role !== 'admin') return res.status(403).send('Access Denied');
     next();
   };
